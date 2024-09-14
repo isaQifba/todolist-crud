cce2ace400c6e7aa625e71c95d2b299d1a28ffc8
Isaque Lis <isaquelis.g@gmail>
2024-09-13 21:15:51 -0300

    mod: toStatus-converter

--- src/main/java/edu/ifba/saj/todo_list/mapper/TarefaMapper.java
+++ src/main/java/edu/ifba/saj/todo_list/mapper/TarefaMapper.java
@@ -2,7 +2,7 @@ package edu.ifba.saj.todo_list.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+import edu.ifba.saj.todo_list.constants.StatusENUM;
 import edu.ifba.saj.todo_list.domain.dto.TarefaDTO;
 import edu.ifba.saj.todo_list.domain.entity.Tarefa;
 
@@ -12,6 +12,10 @@ public abstract class TarefaMapper {
     
     public abstract TarefaDTO toTarefaDTO(Tarefa tarefa);
     
-    @Mapping(target = "status", expression = "java(edu.ifba.saj.todo_list.constants.StatusENUM.fromString(tarefaDTO.getStatus()))")
+    @Mapping(target = "status", expression = "java(toStatusENUM(tarefaDTO.getStatus()))")
     public abstract Tarefa toTarefa(TarefaDTO tarefaDTO);
+
+    public StatusENUM toStatusENUM(String s){
+        return StatusENUM.fromString(s);
+    }
 }
