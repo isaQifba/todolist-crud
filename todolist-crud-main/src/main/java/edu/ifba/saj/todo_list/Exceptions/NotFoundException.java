6c277ca43559d274d036659b7654dc253283a7df
P3iX0nauta <108437786+P3iX0nauta@users.noreply.github.com>
2024-09-21 20:44:58 -0300

    Delete todolist-crud-main directory

--- todolist-crud-main/src/main/java/edu/ifba/saj/todo_list/Exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package edu.ifba.saj.todo_list.Exceptions;
-
-public class NotFoundException extends RuntimeException {
-    
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- todolist-crud-main/src/main/java/edu/ifba/saj/todo_list/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ifba.saj.todo_list.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.ifba.saj.todo_list.domain.entity.Tarefa;
-
-public interface  TarefaRepository extends JpaRepository<Tarefa, Long> {
-    
-}
